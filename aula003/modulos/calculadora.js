a3fe92830bdfc7756817e6e010ab09ba3294517f
Vinicius Santos <vo70346@gmail.com>
2022-08-01 11:19:45 -0300

    pequenas alterações

--- aula003/modulos/calculadora.js
+++ aula003/modulos/calculadora.js
@@ -5,8 +5,77 @@
     Versão: 1.0
 */
 
-//Método tradicional de se criar uma função
-function calcular(valor1, valor2, opcaoCalculo) {
+// Método tradicional de se criar uma função
+    // Formato menos utilizado no JS
+// function calcular(valor1, valor2, opcaoCalculo) {
+//     // Criando variáveis locais para receber o conteúdo dos argumentos que foram encaminhandos na function
+//     let numero1 = valor1;
+//     let numero2 = valor2;
+//     let operacao = opcaoCalculo.toUpperCase();
+//     let resultado;
+//     let erro = false;
+
+//     // isNaN() --> função para validar se o conteúdo de uma variável é número ou não
+//     if(isNaN(numero1) || isNaN(numero2)) {
+//         console.log('ERRO: A informação inserida não é um número!');
+//         erro = true;
+//     } else {
+
+//         /*
+//         if(operacao == 'SOMAR' || operacao == '+') {
+//             resultado = numero1 + numero2;    
+//         } else if(operacao == 'SUBTRAIR' || operacao == '-') {
+//             resultado = numero1 - numero2;
+//         } else if(operacao == 'MULTIPLICAR' || operacao == '*') {
+//             resultado = numero1 * numero2;
+//         } else if(operacao == 'DIVIDIR' || operacao == '/') {
+//             resultado = numero1 / numero2;
+//         } else {
+//             resultado = 'ERRO: Não foi escolhida uma operação válida!';
+//             erro = true;
+//         }
+//         */
+
+//         switch (operacao) {
+//             case 'SOMAR': case '+':
+//                 // if(isNaN(numero1) || isNaN(numero2)) {
+//                 //     resultado = 'ERRO: O valor inserido não é um número'
+//                 //     erro = true;
+//                 // } else {
+//                 //     resultado = numero1 + numero2;
+//                 // }
+//                 resultado = numero1 + numero2;
+//                 break;
+//             case 'SUBTRAIR': case '-':
+//                 resultado = numero1 - numero2;
+//                 break;
+//             case 'MULTIPLICAR': case '*':
+//                 resultado = numero1 * numero2;
+//                 break;
+//             case 'DIVIDIR': case '/':
+//                 if(numero2 == 0) {
+//                     console.log('ERRO: Não é possível realizar a divisão por zero!')
+//                     erro = true;
+//                 } else{
+//                     resultado = numero1 / numero2;
+//                 }
+//                 break;
+//             default:
+//                 console.log('ERRO: Não foi escolhida uma operação válida!');
+//                 erro = true;
+//         }
+//     }
+    
+//     if(erro) {
+//         return false;
+//     } else {
+//         return resultado;
+//     }
+// }
+
+// Método de criação de função utilizando o padrão de call back
+    // Formato bastante utilizado
+const calcular = function(valor1, valor2, opcaoCalculo) {
     // Criando variáveis locais para receber o conteúdo dos argumentos que foram encaminhandos na function
     let numero1 = valor1;
     let numero2 = valor2;
