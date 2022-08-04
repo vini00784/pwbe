e30a973a65008d30ac123b34bf8a8b3279d6561f
Vinicius Santos <vo70346@gmail.com>
2022-08-04 08:56:07 -0300

    aula 05

---
+++ aula005/app.js
@@ -0,0 +1,17 @@
+/*
+    Objetivo:     Aplicar estruturas de repetição (While e For)
+    Autor:        Vinícius Santos Oliveira
+    Data Criação: 04/08/2022
+    Versão:       1.0
+*/
+
+console.log("Calcular uma Tabuada Simples\n")
+
+// Import da biblioteca da entrada de dados
+var readline = require('readline')
+
+// Instância do objeto entradaDados
+var entradaDados = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
