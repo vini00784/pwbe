86e3e966f951b5a0acdde81651fac700670b5814
Vinicius Santos <vo70346@gmail.com>
2022-08-04 10:42:32 -0300

    exercicio 01

--- exercicios/exercicio01.js
+++ exercicios/exercicio01.js
@@ -1,3 +1,15 @@
 /*
-
+    Objetivo:     
+    Autor:        Vinícius Santos Oliveira
+    Data Criação: 04/08/2022
+    Versão:       1.0
 */
+
+var readline = require('readline')
+
+var entradaDados = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+entradaDados.question()
