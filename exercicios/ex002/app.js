5e2cd7d4db704f51238b0da58bc7e58e31afa5c1
Vinicius Santos <vo70346@gmail.com>
2022-08-08 11:14:36 -0300

    exercicio 2 em andamento

--- exercicios/ex002/app.js
+++ exercicios/ex002/app.js
@@ -4,7 +4,7 @@
     Data Criação: 08/08/2022
     Versão: 1.0
 */
-console.log('Calculadora - TABUADA\n')
+console.log('\nCalculadora - TABUADA\n')
 
 const { calcularTabuada } = require('./modules/tabuada.js')
 
@@ -14,4 +14,22 @@ var readline = require('readline')
 var entradaDados = readline.createInterface({
     input: process.stdin,
     output: process.stdout
+})
+
+entradaDados.question('Digite o primeiro valor a ser calculado: ', function(valor1) {
+    let numero1 = parseFloat(valor1)
+
+    entradaDados.question('Digite o segundo número a ser calculado: ', function(valor2) {
+        let numero2 = parseFloat(valor2)
+
+        entradaDados.question('Insira o contador inicial: ', function(valor3) {
+            let contador1 = parseFloat(valor3)
+
+            entradaDados.question('Insira o contador final: ', function(valor4) {
+                let contador2 = parseFloat(valor4)
+
+                calcularTabuada(numero1, numero2, contador1, contador2)
+            })
+        })
+    })
 })
--- exercicios/ex002/modules/tabuada.js
+++ exercicios/ex002/modules/tabuada.js
@@ -5,8 +5,38 @@
     Versão:       1.0
 */
 
-const calcularTabuada = function(numero1, numero2) {
-    
+const calcularTabuada = function(valorInicial, valorFinal, contador1, contador2) {
+    let numero1 = valorInicial
+    let numero2 = valorFinal
+    let contadorInicial = contador1
+    let contadorFinal = contador2
+    let resultado
+    let erro = false
+
+    if(numero1 == '' || numero2  == ''|| contador1 == '' || contador2  == '') {
+        console.log('\nERRO! Preencha todos os campos!\n')
+        erro = true
+    } else if(numero1 < 2 || numero1  > 100 || numero2 < 2 || numero2  > 100) {
+        console.log('\nERRO! Serão válidos apenas números entre 2 e 100\n')
+        erro = true
+    } else if(contador1 < 1 || contador1 > 50 || contador2 < 1 || contador2 > 50) {
+        console.log('\nERRO! O contador deve estar entre 1 e 50!\n')
+        erro = true
+    } else {
+        while(true) {
+            resultado = numero1 * contadorInicial
+            console.log(resultado)
+        }
+    }
+
+
+
+
+    if(erro) {
+        return false
+    } else {
+        return resultado.toFixed(1)
+    }
 }
 
 
