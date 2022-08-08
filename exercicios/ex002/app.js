1f2aeb0922f6dfe1098062cdadc8b52f17d1832c
Vinicius Santos <vo70346@gmail.com>
2022-08-08 09:44:22 -0300

    início exercício 2

--- exercicios/ex002/app.js
+++ exercicios/ex002/app.js
@@ -1,6 +1,17 @@
 /*
     Objetivo:     Criar um sistema que gerencia cálculo de tabuada
     Autor:        Vinícius Santos Oliveira
-    Data Criação: 01/08/2022
+    Data Criação: 08/08/2022
     Versão: 1.0
 */
+console.log('Calculadora - TABUADA\n')
+
+const { calcularTabuada } = require('./modules/tabuada.js')
+
+const { read } = require('fs')
+var readline = require('readline')
+
+var entradaDados = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
---
+++ exercicios/ex002/modules/tabuada.js
@@ -0,0 +1,17 @@
+/*
+    Objetivo:     Criação de funções para cálculo de tabuada
+    Autor:        Vinícius Santos Oliveira
+    Data Criação: 08/08/2022
+    Versão:       1.0
+*/
+
+const calcularTabuada = function(numero1, numero2) {
+    
+}
+
+
+
+
+module.exports = {
+    calcularTabuada
+}
