b26de69f78fda0a4d841d45f13e9937e013c6368
Vinicius Santos <vo70346@gmail.com>
2022-10-03 11:07:57 -0300

    preparação do ambiente

---
+++ aula016/app.js
@@ -0,0 +1,28 @@
+/*
+    Objetivo:     C
+    Autor:        Vinícius Santos Oliveira
+    Data Criação: 03/10/2022
+    Versão:       1.0
+*/
+
+// Comandos a serem rodados além do express, cors e body-parser:
+    // npm install prisma --save
+    // npx prisma
+    // npx prisma init
+        // Eles preparam o ambiente de utilização do Prisma
+
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+// Import das Funções
+
+const app = express()
+
+app.use((request, response, next) => {
+    response.header('Access-Control-Allow-Origin', '*')
+    response.header('Access-Control-Allow-Methos', 'GET, POST, PUT, DELETE, OPTIONS')
+
+    app.use(cors())
+    next()
+})
