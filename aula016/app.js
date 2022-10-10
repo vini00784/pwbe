9fad7319e3c9fdf4d954bb4cc3b78dd9f3277ced
Vinicius Santos <vo70346@gmail.com>
2022-10-10 08:05:41 -0300

    anotações

--- aula016/app.js
+++ aula016/app.js
@@ -1,21 +1,24 @@
 /*
-    Objetivo:     C
+    Objetivo:     API responsável pela manipulação dos dados do Back-End
+                  (GET, POST, PUT, DELETE)
     Autor:        Vinícius Santos Oliveira
     Data Criação: 03/10/2022
     Versão:       1.0
-*/
+    
+    Anotações:
 
-// Comandos a serem rodados (nessa sequência) além do express, cors e body-parser:
-    // O prisma é necessário para a manipulação do acesso ao Banco de Dados
-        // npm install prisma --save
-        // npx prisma
-        // npx prisma init
-        // npm install @prisma/client
-            // Eles preparam o ambiente de utilização do Prisma
-            // npx prisma migrate dev --> comando que faz uma ligação do prisma com o banco, é feito para testar se o prisma realmente consegue acessar o banco
-                // É importante rodar esse comando no início do projeto para ver se está dando tudo certo
-                // O sucesso da criação de uma tabela de teste mostra que a interação com o banco está acontecendo.
-                    // OBS.: Esse comando de teste limpa tudo o que tem no banco, por isso ele deve ser rodada NO INÍCIO DO PROJETO!!!
+    Comandos a serem rodados (nessa sequência) além do express, cors e body-parser:
+        O prisma é necessário para a manipulação do acesso ao Banco de Dados
+            npm install prisma --save
+            npx prisma
+            npx prisma init
+            npx install @prisma/client
+                Eles preparam o ambiente de utilização do Prisma
+                npx prisma migrate dev --> comando que faz uma ligação do prisma com o banco, é feito para testar se o prisma realmente consegue acessar o banco
+                    É importante rodar esse comando no início do projeto para ver se está dando tudo certo
+                    O sucesso da criação de uma tabela de teste mostra que a interação com o banco está acontecendo.
+                        OBS.: Esse comando de teste limpa tudo o que tem no banco, por isso ele deve ser rodada NO INÍCIO DO PROJETO!!!
+*/
 
 const express = require('express')
 const cors = require('cors')
