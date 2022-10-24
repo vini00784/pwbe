c841fbf5462912e4c694cba0f79c4a35214662e0
Vinicius Santos <vo70346@gmail.com>
2022-10-24 07:35:02 -0300

    novas mensagens de erro

--- aula016/module/config.js
+++ aula016/module/config.js
@@ -11,7 +11,8 @@ const MESSAGE_ERROR = {
     INCORRECT_CONTENT_TYPE: 'O cabeçalho da requisição não possui um Content-Type válido!',
     EMPTY_BODY: 'O body da requisição não pode ser vazio',
     NOT_FOUND_DB: 'Não foram encontrados registros no Banco de Dados!',
-    INTERNAL_ERROR_DB: 'Não foi possível realizar a operação com o banco de dados'
+    INTERNAL_ERROR_DB: 'Não foi possível realizar a operação com o banco de dados',
+    REQUIRED_ID: 'O id do registro é obrigatório neste tipo de requisição!'
 }
 
 const MESSAGE_SUCCESS = {
