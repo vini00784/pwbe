390992070d67a068f78581b96d2b447aa4788873
vini00784 <vo70346@gmail.com>
2022-10-27 09:41:45 -0300

    model para listar cursos

--- aula016/model/DAO/curso.js
+++ aula016/model/DAO/curso.js
@@ -1,7 +1,7 @@
 /*
     Objetivo:     Arquivo responsável pela manipulação dos dados dos cursos com o Banco de Dados (insert, update, delete e select)
     Autor:        Vinícius Santos Oliveira
-    Data Criação: 06/10/2022
+    Data Criação: 27/10/2022
     Versão:       1.0
 */
 
@@ -9,8 +9,8 @@ const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
-const insertCourse = async () => {
-
+const insertCourse = async (course) => {
+    
 }
 
 const updateCourse = async () => {
@@ -22,9 +22,21 @@ const deleteCourse = async () => {
 }
 
 const selectAllCourses = async () => {
+    let sql = 'select cast(id as float) as id, nome, carga_horaria, icone, sigla from tbl_curso order by id desc'
 
+    const rsCourses = await prisma.$queryRawUnsafe(sql)
+
+    if(rsCourses.length > 0) {
+        return rsCourses
+    } else {
+        return false
+    }
 }
 
 const selectCourseById = async () => {
 
+}
+
+module.exports = {
+    insertCourse
 }
