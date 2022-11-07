f7d1bde07c15ed115b91bf362fe07c4f24e38cea
vini00784 <vo70346@gmail.com>
2022-11-07 07:42:26 -0300

    function to select student_course

--- aula016/model/DAO/aluno_curso.js
+++ aula016/model/DAO/aluno_curso.js
@@ -43,6 +43,33 @@ const insertStudentCourse = async (studentCourse) => {
 
 }
 
+
+// Função para 
+const selectStudentCourse = async (studentId) => {
+    try {
+        let sql = `select cast(tbl_curso.id as float) as id_curso, tbl_curso.nome as nome_curso, tbl_curso.sigla as sigla_curso, 
+                          tbl_curso.carga_horaria, 
+                          tbl_aluno_curso.matricula, tbl_aluno_curso.status_aluno
+                    from tbl_aluno 
+                        inner join tbl_aluno_curso
+                            on tbl_aluno.id = tbl_aluno_curso.id_aluno
+                        inner join tbl_curso
+                            on tbl_curso.id = tbl_aluno_curso.id_curso
+                        where tbl_aluno.id = ${studentId}`
+
+        const rsStudentCourse = await prisma.$queryRawUnsafe(sql)
+
+        if(rsStudentCourse.length > 0) {
+            return rsStudentCourse
+        } else {
+            return false
+        }
+    } catch(error) {
+        return false
+    }
+} 
+
 module.exports = {
-    insertStudentCourse
+    insertStudentCourse,
+    selectStudentCourse
 }
